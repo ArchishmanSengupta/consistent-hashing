4c0eb75fea3c8e4e2ed7a2371421db811443ca9e
Archishman Sengupta <archishman1808@gmail.com>
2024-07-06 23:11:43 +0530

    chore: ch core functions

--- main/main.go
+++ main/main.go
@@ -15,3 +15,16 @@ package main
 //		- loadMap sync.Map -> map of host to Host struct
 // 		- totalLoad int64 -> Total Load accross all hosts
 //		- hostList []string -> List of all hosts ['uat-server.something.com', 'be-server.something.com']
+
+// Todo: Core functions:
+/*
+-> NewWithConfig: Initializes a new Consistent Hashing instance
+1. Add: Adds Hosts to the Hash Ring
+2. Get: Map a key(user) to a host
+3. GetLeast: Gets least loaded host for a key
+4. IncreaseLead: Increase Load of a particular server
+5. UpdateLoad: Update load of a particular server
+6. DecreaseLoad: Decrease load of a particular server
+7. Remove: Remove a server from the Hash Ring
+8. GetLoads: Get current loads of all hosts
+*/
